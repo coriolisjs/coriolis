0c6f85ee9029396810474d25539d2a30b02f9d8e
Nicolas Deveaud <nicolas@deveaud.fr>
2019-09-06 22:49:23 +0200

    Fix parallel so every operator is connected to source at the same time

--- src/lib/rx/operator/parallel.js
+++ src/lib/rx/operator/parallel.js
@@ -1,7 +1,13 @@
-import { merge } from 'rxjs'
+import { Observable, Subject, merge } from 'rxjs'
 
-export const parallel = combine => (...operators) => source => combine(
-  ...operators.map(operator => source.pipe(operator))
-)
+export const parallel = combine => (...operators) => source =>
+  Observable.create(observer => {
+    const broadcaster = new Subject()
+    combine(
+      ...operators.map(operator => broadcaster.pipe(operator))
+    )
+      .subscribe(observer)
+      .add(source.subscribe(broadcaster))
+  })
 
 export const parallelMerge = parallel(merge)
