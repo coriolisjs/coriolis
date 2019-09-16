38878bba5736db187aeebdc891c969f27ea7f5b6
Nicolas Deveaud <nicolas@deveaud.fr>
2019-09-17 00:18:02 +0200

    Improve log effect and tools

--- examples/todos/src/effects/logEvents.js
+++ examples/todos/src/effects/logEvents.js
@@ -1,18 +1,22 @@
+import { truncate } from '../libs/string/truncate'
 
 const typeStyle = 'color: silver; font-weight: bold'
+const trunc = truncate(24)
 
-const norm = type => {
-  if (type.length > 20) {
-    return `${type.slice(0, 20)}... `
-  }
-
-  return type.padEnd(24, ' ')
-}
+const log = (...args) => console.log(...args)
+const logInfo = (...args) => console.info(...args)
+const logError = (...args) => console.error(...args)
 
 const logSubscriber = (prefix, ...args) => ({
-  next: ({ type, payload }) => console.log(`${prefix} %c ${norm(type)}`, ...args.slice(0, 1), typeStyle, payload, ...args.slice(1)),
-  error: error => console.error(`${prefix} error`, ...args, error),
-  complete: () => console.info(`Completed ${prefix}`, ...args)
+  next: ({ type, payload }) => log(
+    `${prefix} %c ${trunc(type)}`,
+    ...args.slice(0, 1),
+    typeStyle,
+    payload,
+    ...args.slice(1)
+  ),
+  error: error => logError(`${prefix} error`, ...args, error),
+  complete: () => logInfo(`Completed ${prefix}`, ...args)
 })
 
 export const logEvents = ({ initialEvent$, addLogger }) => {
@@ -20,6 +24,8 @@ export const logEvents = ({ initialEvent$, addLogger }) => {
   const initialEventsSubscription = initialEvent$.subscribe(logSubscriber('🌀'))
 
   return () => {
+    // not using .add() here to join both tear-downs, to avoid initialEvents completion to
+    // cause call of logger tear-down
     initialEventsSubscription.unsubscribe()
     removeLogger()
   }
---
+++ examples/todos/src/libs/string/truncate.js
@@ -0,0 +1,10 @@
+export const truncate = size => {
+  const short = size - 4
+  return str => {
+    if (str.length > short) {
+      return `${str.slice(0, short)}... `
+    }
+
+    return str.padEnd(size, ' ')
+  }
+}
