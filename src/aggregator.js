f6aa7728617b374b3b1f3abad5ea54a2340ac131
Nicolas Deveaud <nicolas@deveaud.fr>
2019-09-19 17:10:20 +0200

    Improve readability for aggregator factory

--- src/aggregator.js
+++ src/aggregator.js
@@ -1,6 +1,6 @@
 import { identity, noop } from 'rxjs'
 
-export const createReducerAggregator = reducer => {
+const createReducerAggregator = reducer => {
   if (typeof reducer !== 'function') {
     throw new TypeError('reducer must be a function')
   }
@@ -22,43 +22,10 @@ export const createReducerAggregator = reducer => {
   }
 }
 
-export const createComplexAggregator = (setupAggregator, getAggregator) => {
-  if (typeof setupAggregator !== 'function') {
-    throw new TypeError('setupAggregator must be a function')
-  }
-
-  let lastValues = []
-  let aggrBehaviour = noop
-
+const handleAggregatorSetup = (getLastState, getAggregator) => {
   const using = {
     aggr: []
   }
-
-  const getValues = event => {
-    const values = using.aggr.map(aggr => aggr(event))
-
-    if (using.event) {
-      return values
-    }
-
-    const anyChange = values.some((item, idx) => item !== lastValues[idx])
-
-    return anyChange ? values : lastValues
-  }
-
-  const aggregate = createReducerAggregator((lastState, event) => {
-    const values = getValues(event)
-
-    if (values === lastValues) {
-      return lastState
-    }
-
-    lastValues = values
-
-    return aggrBehaviour(...values)
-  })
-
-  const getLastState = () => aggregate()
 
   const useState = () => {
     using.state = true
@@ -73,35 +40,94 @@ export const createComplexAggregator = (setupAggregator, getAggregator) => {
   const useAggr = aggr =>
     using.aggr.push(getAggregator(aggr))
 
+  const setupParams = {
+    useState,
+    useEvent,
+    useAggr
+  }
+
+  const isNullSetup = () => using.aggr.length === 0
+
+  const isReducerSetup = () =>
+    (!using.aggr[0] || using.aggr[0] === getLastState) &&
+    (!using.aggr[1] || using.aggr[1] === identity)
+
+  const isReducerLikeSetup = () =>
+    using.aggr.length === (using.state + using.event)
+
+  let lastValues = []
+  const getValues = event => {
+    const values = using.aggr.map(aggr => aggr(event))
+
+    if (using.event) {
+      return values
+    }
+
+    const anyChange = values.some((item, idx) => item !== lastValues[idx])
+    lastValues = values
+
+    return anyChange ? values : undefined
+  }
+
+  return {
+    setupParams,
+    getValues,
+    isNullSetup,
+    isReducerSetup,
+    isReducerLikeSetup
+  }
+}
+
+const createComplexAggregator = (setupAggregator, getAggregator) => {
+  if (typeof setupAggregator !== 'function') {
+    throw new TypeError('setupAggregator must be a function')
+  }
+
+  let aggrBehaviour
+
+  let aggregate = noop
+  const getLastState = () => aggregate()
+
+  const {
+    setupParams,
+    getValues,
+    isNullSetup,
+    isReducerSetup,
+    isReducerLikeSetup
+  } = handleAggregatorSetup(getLastState, getAggregator)
+
   try {
-    aggrBehaviour = setupAggregator({
-      useState,
-      useEvent,
-      useAggr
-    })
+    aggrBehaviour = setupAggregator(setupParams)
   } catch (error) {
     // setup failed, let's assume setupAggregator is in fact a reducer
     return createReducerAggregator(setupAggregator)
   }
 
-  if (using.aggr.length === 0 || typeof aggrBehaviour !== 'function') {
+  if (isNullSetup() || typeof aggrBehaviour !== 'function') {
     // giving a reducer with optional parameters could lead here.
     // let's assume setupAggregator is in fact a reducer
     return createReducerAggregator(setupAggregator)
   }
 
   // if given aggregator definition expects only state and event (or less), it should be a reducer
-  if (using.aggr.length === (using.state + using.event)) {
-    console.warn('Prefer using reducer when only needs state and/or event')
+  if (isReducerLikeSetup()) {
+    console.warn('Prefer using simple reducer signature when you only need state and/or event')
 
     // Replace with getAggregator in case signature matches reducer signature (state, event)
-    if (
-      (!using.aggr[0] || using.aggr[0] === getLastState) &&
-      (!using.aggr[1] || using.aggr[1] === identity)
-    ) {
+    if (isReducerSetup()) {
       return getAggregator(aggrBehaviour)
     }
   }
+
+  aggregate = createReducerAggregator((lastState, event) => {
+    const values = getValues(event)
+
+    if (!values) {
+      return lastState
+    }
+
+    return aggrBehaviour(...values)
+  })
 
   return aggregate
 }
