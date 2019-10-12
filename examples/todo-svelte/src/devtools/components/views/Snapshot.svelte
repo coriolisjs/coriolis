ae3f161e676ee9b6ff7adbf5a05a75cb73813f0d
Nicolas Deveaud <nicolas@deveaud.fr>
2019-10-13 00:34:36 +0200

    Improve snapshot view display

--- examples/todo-svelte/src/devtools/components/views/Snapshot.svelte
+++ examples/todo-svelte/src/devtools/components/views/Snapshot.svelte
@@ -6,6 +6,13 @@
   const snapshot$ = getContext('snapshot$')
 </script>
 
+<style>
+  pre {
+    height: 100%;
+    overflow: auto;
+  }
+</style>
+
 <DevToolsContainer>
   <h2 slot="title">Snapshot</h2>
   <pre>{JSON.stringify($snapshot$, null, 2)}</pre>
