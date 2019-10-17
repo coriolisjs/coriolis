0497e788591ca5a0effc651b158f07c740a644d9
Nicolas Deveaud <nicolas@deveaud.fr>
2019-10-17 23:59:27 +0200

    EventTypeList as a unit component

---
+++ examples/todo-svelte/src/devtools/components/units/EventTypeList.svelte
@@ -0,0 +1,23 @@
+<script>
+  import { getContext } from 'svelte'
+
+  import { eventTypeList } from '../../aggrs/eventTypeList'
+
+  const getSource = getContext('getSource')
+
+  let eventTypeList$ = getSource(eventTypeList)
+</script>
+
+<style lang="scss">
+  .event-type-list {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  }
+</style>
+
+<ul class="event-type-list">
+  {#each $eventTypeList$ as eventType (eventType.name)}
+    <li>{eventType.name} - {eventType.count}</li>
+  {/each}
+</ul>
--- examples/todo-svelte/src/devtools/components/views/EventTypeList.svelte
+++ examples/todo-svelte/src/devtools/components/views/EventTypeList.svelte
@@ -1,28 +1,9 @@
 <script>
-  import { getContext } from 'svelte'
-
   import DevToolsContainer from '../units/DevToolsContainer.svelte'
-
-  import { eventTypeList } from '../../aggrs/eventTypeList'
-
-  const getSource = getContext('getSource')
-
-  let eventTypeList$ = getSource(eventTypeList)
+  import EventTypeList from '../units/EventTypeList.svelte'
 </script>
-
-<style lang="scss">
-  .event-type-list {
-    list-style: none;
-    margin: 0;
-    padding: 0;
-  }
-</style>
 
 <DevToolsContainer>
   <h2 slot="title">Liste des types d'events</h2>
-  <ul class="event-type-list">
-    {#each $eventTypeList$ as eventType (eventType.name)}
-      <li>{eventType.name} - {eventType.count}</li>
-    {/each}
-  </ul>
+  <EventTypeList />
 </DevToolsContainer>
