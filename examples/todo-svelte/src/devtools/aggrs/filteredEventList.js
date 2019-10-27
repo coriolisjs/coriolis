3e82f69c57aab215d273084198fa1c9797c99559
Nicolas Deveaud <nicolas@deveaud.fr>
2019-10-27 23:17:05 +0100

    coriolis devtools have it's own events store

--- examples/todo-svelte/src/devtools/aggrs/filteredEventList.js
+++ examples/todo-svelte/src/devtools/aggrs/filteredEventList.js
@@ -1,21 +1,8 @@
 import { eventList } from './eventList'
 import { eventListFilter } from './eventListFilter'
-import * as events from '../events'
-
-const devtoolsEventNames = Object.values(events)
-  .map(eventBuilder => eventBuilder.toString())
-  .reduce((acc, type) => ({
-    ...acc,
-    [type]: true
-  }), {})
-
-const subjectEventList = ({ useAggr }) => (
-  useAggr(eventList),
-  eventList => eventList.filter(event => !devtoolsEventNames[event.type])
-)
 
 export const filteredEventList = ({ useAggr }) => (
-  useAggr(subjectEventList),
+  useAggr(eventList),
   useAggr(eventListFilter),
   (events, filter) => filter
     ? events.filter(event => event.type.includes(filter))
--- examples/todo-svelte/src/devtools/effect.js
+++ examples/todo-svelte/src/devtools/effect.js
@@ -1,83 +1,43 @@
 import { EMPTY } from 'rxjs'
-import { debounceTime } from 'rxjs/operators'
+
+import { createStore } from 'coriolis'
 
 import { createUI } from './effects/ui'
 import { createNav } from './effects/nav'
 
 import { viewNames } from './components/views'
 
-export const createCoriolisDevToolsEffect = (aggregatorEvents = EMPTY) => ({ addEffect, eventSource, initialEvent$ }) => {
-  const removeUI = addEffect(createUI())
-  const removeNav = addEffect(createNav(viewNames))
+const initDevtoolsEventStore = () => {
+  let devtoolsEventSource
 
-  const aggregatorEventList = []
-  let aggregatorEventsState = {}
-  // const subscription1 = aggregatorEvents
-  //   .subscribe(event => aggregatorEventList.push(event))
-
-  const subscription4 = initialEvent$.subscribe(() => {
-    aggregatorEventsState = {
-      ...aggregatorEventsState,
-      eventsCount: (aggregatorEventsState.eventsCount || 0) + 1
-    }
+  createStore(
+    createUI(),
+    createNav(viewNames),
+    ({ eventSource }) => {
+    devtoolsEventSource = eventSource
   })
 
-  const subscription3 = eventSource.subscribe(() => {
-    aggregatorEventsState = {
-      ...aggregatorEventsState,
-      eventsCount: (aggregatorEventsState.eventsCount || 0) + 1
+  createDevtoolsEffect = (aggregatorEvents = EMPTY) => ({ eventSource, initialEvent$ }) => {
+    const aggregatorEventsSubscription = aggregatorEvents.subscribe(event => devtoolsEventSource.next(event))
+    const initialEventsSubscription = initialEvent$.subscribe(event => devtoolsEventSource.next(event))
+    const eventsSubscription = eventSource.subscribe(event => devtoolsEventSource.next(event))
+    eventStoresCount
+
+    return () => {
+      aggregatorEventsSubscription.unsubscribe()
+      initialEventsSubscription.unsubscribe()
+      eventsSubscription.unsubscribe()
     }
-  })
-
-  const subscription1 = aggregatorEvents
-    .subscribe(event => {
-      const aggrName = (event.payload && event.payload.name) || 'unnamed'
-      const byType = aggregatorEventsState[event.type] || {}
-      const byTypeAndAggr = byType[aggrName]
-
-      aggregatorEventsState = {
-        ...aggregatorEventsState,
-        [event.type]: {
-          ...byType,
-          [aggrName]: (byTypeAndAggr || 0) + 1,
-          __count: (byType.__count || 0) + 1
-        },
-        __count: (aggregatorEventsState.__count || 0) + 1
-      }
-    })
-
-  const subscription2 = aggregatorEvents
-    .pipe(
-      debounceTime(1000)
-    )
-    .subscribe(() => {
-      // console.log(aggregatorEventList.length)
-      // console.log(aggregatorEventList.reduce((acc, event) => ({
-      //   ...acc,
-      //   [event.type]: (acc[event.type] || 0) + 1
-      // }), {}))
-
-      // console.log(aggregatorEventList
-      //   // .filter(event => event.type === 'Coriolis devtools detected an aggr have been called')
-      //   .reduce((acc, event) => ({
-      //     ...acc,
-      //     [event.type]: {
-      //       ...acc[event.type],
-      //       [event.payload && event.payload.name]: (acc[event.type] && acc[event.type][event.payload && event.payload.name] || 0) + 1,
-      //       __count: ((acc[event.type] && acc[event.type].__count) || 0) + 1
-      //     }
-      //   }), {}))
-      console.log(aggregatorEventsState)
-    })
-    // .subscribe(event => console.log(event))
-    // .subscribe(eventSource)
-
-  return () => {
-    removeUI()
-    removeNav()
-    subscription1.unsubscribe()
-    subscription2.unsubscribe()
-    subscription3.unsubscribe()
-    subscription4.unsubscribe()
   }
+
+  return createDevtoolsEffect
+}
+
+let createDevtoolsEffect
+export const createCoriolisDevToolsEffect = aggregatorEvents => {
+  if (!createDevtoolsEffect) {
+    createDevtoolsEffect = initDevtoolsEventStore()
+  }
+
+  return createDevtoolsEffect(aggregatorEvents)
 }
