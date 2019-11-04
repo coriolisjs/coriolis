ef7a7db7b3db248f72a009a91fce9d50bba7372e
Nicolas Deveaud <nicolas@deveaud.fr>
2019-11-05 00:08:21 +0100

    Includes coriolis dev tools into todo-vue example

--- examples/todo-vuejs/src/main.js
+++ examples/todo-vuejs/src/main.js
@@ -3,11 +3,14 @@ import { createStore } from 'coriolis'
 import { createUi } from './effects/ui'
 import { localStorage } from './effects/localStorage'
 import { logEvents } from './effects/logEvents'
+import { wrapCoriolisOptions } from 'coriolis-dev-tools'
 
 const storageKey = 'storedEventsForTodoListApp'
 
 createStore(
-  logEvents,
-  localStorage(storageKey),
-  createUi()
+  wrapCoriolisOptions(
+    logEvents,
+    localStorage(storageKey),
+    createUi()
+  )
 )
