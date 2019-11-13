ad8c9d0db9a375b19b88e81cc7487e97a7d5c46d
Nicolas Deveaud <nicolas@deveaud.fr>
2019-11-13 14:06:30 +0100

    Remove useless todo

--- tools/coriolis-dev-tools/src/aggrs/isDevtoolsOpen.js
+++ tools/coriolis-dev-tools/src/aggrs/isDevtoolsOpen.js
@@ -1,6 +1,5 @@
 import { devtoolsOpened, devtoolsClosed } from '../events'
 
-// TODO: events could be converted into one unique event for both open and close
 export const isDevtoolsOpen = ({ useEvent }) => (
   useEvent(devtoolsOpened, devtoolsClosed),
   event => event.type === devtoolsOpened.toString()
