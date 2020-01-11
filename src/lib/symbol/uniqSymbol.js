eae4fa28e4cc1c0b0ec923bc82c0a0ecc7291974
Nicolas Deveaud <nicolas@deveaud.fr>
2020-01-11 23:08:35 +0100

    UniqSymbol: comment and makes more debug friendly

--- src/lib/symbol/uniqSymbol.js
+++ src/lib/symbol/uniqSymbol.js
@@ -1,1 +1,4 @@
-export const uniqSymbol = () => Symbol(Math.random().toString(36).substring(2, 15))
+// Symbol is already building "unique" instances.
+// But for debug purpose, a unique "name" helps.
+// This is why this function will create a Symbol instance with a random name
+export const uniqSymbol = () => Symbol('uniqSymbol' + Math.random().toString(36).substring(2, 15))
