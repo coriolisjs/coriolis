67b9eef8d4a9ca22af5eb5b1ef16638da22db248
Nicolas Deveaud <nicolas@deveaud.fr>
2020-02-12 18:44:40 +0100

    Adds example simple count using commonJS

---
+++ examples/count-comonjs/entry.js
@@ -0,0 +1,32 @@
+const { createStore } = require('@coriolis/coriolis')
+
+const currentCount = ({ useState, useEvent }) => (
+  useState(0),
+  useEvent(),
+  (state, event) => {
+    switch (event.type) {
+      case 'incremented':
+        return state + 1
+
+      case 'decremented':
+        return state - 1
+
+      default:
+        return state
+    }
+  }
+)
+
+createStore(({ withProjection, eventSubject }) => {
+  withProjection(currentCount).subscribe(count => console.log(count))
+  // 0
+
+  eventSubject.next({ type: 'incremented' })
+  // 1
+
+  eventSubject.next({ type: 'incremented' })
+  // 2
+
+  eventSubject.next({ type: 'decremented' })
+  // 1
+})
