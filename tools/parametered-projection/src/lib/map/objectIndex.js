6aaee8b550df4ad31bc5d0657de51f3a93fc0034
Nicolas Deveaud <nicolas@deveaud.fr>
2020-03-05 01:13:00 +0100

    Lowers objectIndex code complexity

--- tools/parametered-projection/src/lib/map/objectIndex.js
+++ tools/parametered-projection/src/lib/map/objectIndex.js
@@ -1,44 +1,58 @@
-export const createIndex = getNotYetIndexed => {
-  let index = new Map()
+const createSubsetGetter = (index, getNotYetIndexed) => (
+  indexed,
+  key,
+  rest,
+) => {
+  let subset = indexed.subset
 
-  const get = (key, ...rest) => {
-    const indexed = index.get(key) || {}
-
-    if (rest.length) {
-      let subset = indexed.subset
-
-      if (!subset) {
-        subset = createIndex((...args) => getNotYetIndexed(key, ...args))
-
-        index.set(key, {
-          ...indexed,
-          subset,
-        })
-      }
-
-      return subset.get(...rest)
-    }
-
-    if (indexed.loading) {
-      throw new Error('Cycle index referencing is not possible')
-    }
-
-    if ('value' in indexed) {
-      return indexed.value
-    }
-
-    indexed.loading = true
-
-    const value = getNotYetIndexed(key)
+  if (!subset) {
+    subset = createIndex((...args) => getNotYetIndexed(key, ...args))
 
     index.set(key, {
       ...indexed,
-      value,
-      loading: false,
+      subset,
     })
-
-    return value
   }
+
+  return subset.get(...rest)
+}
+
+const createValueGetter = (
+  index,
+  getNotYetIndexed,
+  getSubset = createSubsetGetter(index, getNotYetIndexed),
+) => (key, ...rest) => {
+  const indexed = index.get(key) || {}
+
+  if (rest.length) {
+    return getSubset(indexed, key, rest)
+  }
+
+  if (indexed.loading) {
+    throw new Error('Cycle index referencing is not possible')
+  }
+
+  if ('value' in indexed) {
+    return indexed.value
+  }
+
+  indexed.loading = true
+
+  const value = getNotYetIndexed(key)
+
+  index.set(key, {
+    ...indexed,
+    value,
+    loading: false,
+  })
+
+  return value
+}
+
+export const createIndex = getNotYetIndexed => {
+  let index = new Map()
+
+  const get = createValueGetter(index, getNotYetIndexed)
 
   const list = () =>
     [...index].reduce(
