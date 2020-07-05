0e84b189166ec76f7e490bec365c863921098932
Nicolas Deveaud <nicolas@deveaud.fr>
2020-07-05 19:25:01 +0200

    Add tests on event libs

--- src/lib/event/isValidEvent.js
+++ src/lib/event/isValidEvent.js
@@ -3,10 +3,12 @@ import { hasOnlyKeys } from '../object/hasOnlyKeys'
 export const isCommand = (command) => typeof command === 'function'
 
 const isEvent = (event) =>
-  event.type &&
-  hasOnlyKeys(event, ['type', 'payload', 'error', 'meta']) &&
-  (event.error === undefined || typeof event.error === 'boolean') &&
-  (event.meta === undefined || typeof event.meta === 'object')
+  !!(
+    event.type &&
+    hasOnlyKeys(event, ['type', 'payload', 'error', 'meta']) &&
+    (event.error === undefined || typeof event.error === 'boolean') &&
+    (event.meta === undefined || typeof event.meta === 'object')
+  )
 
 export const isValidEvent = (event) =>
-  event && (isCommand(event) || isEvent(event))
+  !!event && (isCommand(event) || isEvent(event))
