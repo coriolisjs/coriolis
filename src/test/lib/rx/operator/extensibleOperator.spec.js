b7ae833c87503519092b113a0fdb8475752d4dda
Nicolas Deveaud <nicolas@deveaud.fr>
2020-07-08 09:54:50 +0200

    Adds extensibleOperator rx operator

---
+++ src/test/lib/rx/operator/extensibleOperator.spec.js
@@ -0,0 +1,51 @@
+import { from } from 'rxjs'
+import { map } from 'rxjs/operators'
+
+import { createExtensibleOperator } from '../../../../lib/rx/operator/extensibleOperator'
+
+describe('extensibleOperator', () => {
+  it('should stream values mapped as expected', () => {
+    const source = from('abcdefghijklmnop'.slice())
+
+    const results = []
+    const { operator, add } = createExtensibleOperator()
+
+    const removeZ = add(map((value) => value + 'z'))
+
+    let removeY
+    source.pipe(operator).subscribe((value) => {
+      results.push(value)
+
+      if (value === 'cz') {
+        removeY = add(map((value) => value + 'y'))
+      }
+
+      if (value === 'gzy') {
+        removeZ()
+      }
+
+      if (value === 'ly') {
+        removeY()
+      }
+    })
+
+    expect(results).to.deep.equal([
+      'az',
+      'bz',
+      'cz',
+      'dzy',
+      'ezy',
+      'fzy',
+      'gzy',
+      'hy',
+      'iy',
+      'jy',
+      'ky',
+      'ly',
+      'm',
+      'n',
+      'o',
+      'p',
+    ])
+  })
+})
