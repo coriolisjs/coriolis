7e781e4ee8906c3752850cc2e5b280779bbf50fd
Nicolas Deveaud <nicolas@deveaud.fr>
2021-12-22 20:25:54 +0100

    (timestamp) Fixes deprecated function usage

--- src/lib/time/timestamp.js
+++ src/lib/time/timestamp.js
@@ -1,10 +1,15 @@
 import { hrToMs } from './hrToMs'
 
-const hasPerformance = typeof performance !== 'undefined'
+const hasPerformance =
+  typeof window !== 'undefined' && typeof performance !== 'undefined'
+
 const hasHr = typeof process !== 'undefined' && !!process.hrtime
 
 const dateRef =
-  (hasPerformance && performance.timing.navigationStart) || +new Date()
+  (hasPerformance &&
+    (performance.timeOrigin ||
+      (performance.timing && performance.timing.navigationStart))) ||
+  +new Date()
 
 const timeDeltaRef = hasHr && process.hrtime()
 
