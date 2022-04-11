cd38a81ec35b0fec90155a226db3f0a4c390dc74
Nicolas Deveaud <nicolas@deveaud.fr>
2022-04-11 22:47:21 +0200

    Experiment new store providing solution (next coriolis-svelte ?)

---
+++ examples/todo/src/components/Router.svelte
@@ -0,0 +1,17 @@
+<script>
+  import { withProjection } from '@coriolis/coriolis-svelte'
+  import { currentView } from '../todo-core/projections/currentView'
+
+  export let views
+
+  const viewName$ = withProjection(currentView)
+
+  let CurrentView
+  $: CurrentView = views[$viewName$]
+</script>
+
+{#if CurrentView}
+  <svelte:component this={CurrentView}/>
+{:else}
+  <div>... Routing Error ...</div>
+{/if}
